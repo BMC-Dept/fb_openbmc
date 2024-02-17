b7e6ea183f51cbdfaa084f980b1811788f74924d
Delphine CC Chiu <Delphine_CC_Chiu@wiwynn.com>
2024-02-17 15:38:30 +0800

    yosemite4-n: add firmware update tools and other tools
    Add cxl-update, pldm-fw-update, compute-software-id, bios-fw-update, cpld-fw-handler, loadsvf,
    mfg-tool.
    
    Change-Id: I7b261fad0baa50a36f732355fbbea7f6dfcd661f

---
+++ meta-facebook/meta-yosemite4/meta-yosemite4-n/recipes-facebook/cpld-fw-handler/files/cpld-fw-handler.cpp
@@ -0,0 +1,62 @@
+
+#include "cpld-fw-handler.hpp"
+
+#include <linux/i2c-dev.h>
+#include <linux/i2c.h>
+#include <sys/ioctl.h>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+
+template<typename Func>
+int retryCond(Func func, int numRetries, int msec) {
+    for (int retries = 0; retries < numRetries; retries++) {
+        if (func()) {
+            return 0;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(msec));
+    }
+    return -1;
+}
+
+int CpldManager::i2cWriteReadCmd(const std::vector<uint8_t>& cmdData,
+                                 size_t rx_len, std::vector<uint8_t>& readData)
+{
+    if (debugMode)
+    {
+        std::cout << "[DEBUG] CMD data = ";
+        for (const auto& i : cmdData)
+        {
+            std::cout << std::hex << std::setfill('0') << std::setw(2)
+                      << unsigned(i) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    struct i2c_rdwr_ioctl_data iomsg;
+    struct i2c_msg i2cmsg[2];
+
+    i2cmsg[0].addr = addr & 0xFF;
+    i2cmsg[0].flags = 0;
+    i2cmsg[0].len = cmdData.size();
+    i2cmsg[0].buf = const_cast<uint8_t*>(cmdData.data());
+    iomsg.nmsgs = 1;
+    if (rx_len > 0)
+    {
+        i2cmsg[1].addr = addr & 0xFF;
+        i2cmsg[1].flags = I2C_M_RD;
+        i2cmsg[1].len = rx_len;
+        i2cmsg[1].buf = reinterpret_cast<uint8_t*>(readData.data());
+        iomsg.nmsgs++;
+    }
+    iomsg.msgs = i2cmsg;
+
+    if (retryCond([&]() { return ioctl(i2c_fd, I2C_RDWR, &iomsg); }, 3, 10) < 0)
+    {
+        std::cerr << "Fail to r/w I2C device: " << unsigned(bus)
+                  << ", Addr: " << unsigned(addr) << "errno = " << std::strerror(errno) << "\n";
+        return -1;
+    }
+    return 0;
+}
